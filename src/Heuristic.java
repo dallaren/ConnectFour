a9cb848be2508cc7592c027941f7c787f9f754f7
Mikkel Balslev <mibal@itu.dk>
2017-02-24 10:22:26 +0100

    removed workspace

--- src/Heuristic.java
+++ src/Heuristic.java
@@ -4,12 +4,13 @@
 public class Heuristic {
 
     public int SomeHeuristic(State s){
-        int collumns = 1;
-        int rows = 1;
+        int collumns = s.getCollumns;
+        int rows = s.getRows;
+        int player = s.getPlayer();
         int utility = 0;
         for(int x = 0; x < collumns; x--) {
             for(int y = 0 ; y < rows; y--){
-                utility++;
+
             }
         }
         return utility;
