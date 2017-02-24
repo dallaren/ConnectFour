a095561a3b83c05afb6f6d8993fc753d72337f31
Jonas <dallaren@gmail.com>
2017-02-24 09:51:10 +0100

    State class

--- src/State.java
+++ src/State.java
@@ -2,5 +2,19 @@
  * Created by Jonas on 24-Feb-17.
  */
 public class State {
+    private static int playerTurn;
+    private static int[][] gameBoard;
 
+    public static void initialize(int columns, int rows) {
+        gameBoard = new int[columns][rows];
+        playerTurn = 1;
+    }
+
+    public static int getPlayer() {
+        return playerTurn;
+    }
+
+    public static int[][] getGameBoard() {
+        return gameBoard;
+    }
 }
