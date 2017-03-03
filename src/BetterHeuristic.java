0f12fc5d0bc8ef0208dde701204ed069d21b6cd2
Mikkel Balslev <mibal@itu.dk>
2017-03-03 11:53:56 +0100

    improvement to heuristic
    now only counts one win/loss in each field

--- src/BetterHeuristic.java
+++ src/BetterHeuristic.java
@@ -6,6 +6,7 @@ public class BetterHeuristic implements IHeuristic {
     /**
      * currently only checks for connections downwards
      */
+    private static boolean iWin = false, uWin = false;
     public int getUtility(State s) {
         int columns = s.getColumns();
         int rows = s.getRows();
@@ -14,6 +15,8 @@ public class BetterHeuristic implements IHeuristic {
         int utility = 0;
         for (int x = 0; x < columns; x++) {
             for (int y = 0; y < rows ; y++) {
+                iWin = false;
+                uWin = false;
                 //find the first empty spot in each column
                 if (board[x][y] != 0) continue;
                 //<editor-fold desc = "first check downwards if possible">
@@ -27,102 +30,136 @@ public class BetterHeuristic implements IHeuristic {
                 }
                 if (down!= 0){
                     int sign = (down == player) ? 1 : -1;
-                    utility += (downInRow>=3?sign:0);
+                    //utility += (downInRow>=3?sign:0);
+                    whoWins(sign);
+
                 }
                 //</editor-fold>
                 //<editor-fold desc= "then check left and right if possible">
-                int left = 0, right = 0;
-                int leftInRow = 0, rightInRow = 0;
-                if (x > 0) {
-                    //first check downwards
-                    left = board[x-1][y];}
-                if (x < columns-1) {
-                    //first check downwards
-                    right = board[x+1][y];}
-
-                //check left
-                if (left != 0) {
-                    for (int i = 1; x - i >= 0 && i <= 3; i++) {
-                        if (board[x - i][y] != left) {break;}
-                        leftInRow++;
+                if (!bothWon()) {
+                    int left = 0, right = 0;
+                    int leftInRow = 0, rightInRow = 0;
+                    if (x > 0) {
+                        //first check downwards
+                        left = board[x - 1][y];
                     }
-                }
-                //check right
-                if (right != 0) {
-                    for (int i = 1; x + i < columns && i <= 3; i++) {
-                        if (board[x + i][y] != left) {break;}
-                        rightInRow++;
+                    if (x < columns - 1) {
+                        //first check downwards
+                        right = board[x + 1][y];
                     }
+                    //check left
+                    if (left != 0) {
+                        for (int i = 1; x - i >= 0 && i <= 3; i++) {
+                            if (board[x - i][y] != left) {
+                                break;
+                            }
+                            leftInRow++;
+                        }
+                    }
+                    if
+                    //check right
+                    if (right != 0) {
+                        for (int i = 1; x + i < columns && i <= 3; i++) {
+                            if (board[x + i][y] != left) {
+                                break;
+                            }
+                            rightInRow++;
+                        }
+                    }
+                    somethingInRow(left, right, leftInRow, rightInRow, player);
                 }
-                utility += somethingInRow(left,right,leftInRow,rightInRow,player);
-
                 //</editor-fold>
                 //<editor-fold desc = "then check diagonally">
-                int leftUp = 0, rightUp = 0,leftDown = 0, rightDown = 0;
-                int leftUpInRow = 0, rightUpInRow = 0,leftDownInRow = 0, rightDownInRow = 0;
-                if (x > 0 && y > 0) {
-                    //first check downwards
-                    leftDown = board[x-1][y-1];}
-                if (x < columns-1 && y > 0) {
-                    //first check downwards
-                    rightDown = board[x+1][y-1];}
-                if (x > 0 && y < rows-1) {
-                    //first check downwards
-                    leftUp = board[x-1][y+1];}
-                if (x < columns-1 && y < rows-1) {
-                    //first check downwards
-                    rightUp = board[x+1][y+1];}
-                int temp;
-                //down left
-                if (leftDown != 0) {
-                    for (int i = 1; x - i >= 0 && y - i >= 0 && i <= 3; i++) {
-                        temp = board[x-i][y-i];
-                        if (temp != leftDown) {break;}
-                        leftDownInRow++;
+                if(!bothWon()) {
+                    int leftUp = 0, rightUp = 0, leftDown = 0, rightDown = 0;
+                    int leftUpInRow = 0, rightUpInRow = 0, leftDownInRow = 0, rightDownInRow = 0;
+                    if (x > 0 && y > 0) {
+                        //first check downwards
+                        leftDown = board[x - 1][y - 1];
                     }
-                }
-                //down right
-                if (rightDown != 0) {
-                    for (int i = 1; x + i < columns && y - i >= 0 && i <= 3; i++) {
-                        temp = board[x+i][y-i];
-                        if (temp != rightDown) {break;}
-                        rightDownInRow++;
+                    if (x < columns - 1 && y > 0) {
+                        //first check downwards
+                        rightDown = board[x + 1][y - 1];
                     }
-                }
-                //up left
-                if (leftUp != 0) {
-                    for (int i = 1; x - i >= 0 && y + i < rows && i <= 3; i++) {
-                        if ( board[x-i][y+i] != leftUp) {break;}
-                        leftUpInRow++;
+                    if (x > 0 && y < rows - 1) {
+                        //first check downwards
+                        leftUp = board[x - 1][y + 1];
                     }
-                }
-                //up right
-                if (rightUp != 0) {
-                    for (int i = 1; x + i < columns && y + i < rows && i <= 3; i++) {
-                        if (board[x+i][y+i] != rightDown) {break;}
-                        rightDownInRow++;
+                    if (x < columns - 1 && y < rows - 1) {
+                        //first check downwards
+                        rightUp = board[x + 1][y + 1];
                     }
+                    int temp;
+                    //down left
+                    if (leftDown != 0) {
+                        for (int i = 1; x - i >= 0 && y - i >= 0 && i <= 3; i++) {
+                            temp = board[x - i][y - i];
+                            if (temp != leftDown) {
+                                break;
+                            }
+                            leftDownInRow++;
+                        }
+                    }
+                    //down right
+                    if (rightDown != 0) {
+                        for (int i = 1; x + i < columns && y - i >= 0 && i <= 3; i++) {
+                            temp = board[x + i][y - i];
+                            if (temp != rightDown) {
+                                break;
+                            }
+                            rightDownInRow++;
+                        }
+                    }
+                    //up left
+                    if (leftUp != 0) {
+                        for (int i = 1; x - i >= 0 && y + i < rows && i <= 3; i++) {
+                            if (board[x - i][y + i] != leftUp) {
+                                break;
+                            }
+                            leftUpInRow++;
+                        }
+                    }
+                    //up right
+                    if (rightUp != 0) {
+                        for (int i = 1; x + i < columns && y + i < rows && i <= 3; i++) {
+                            if (board[x + i][y + i] != rightDown) {
+                                break;
+                            }
+                            rightDownInRow++;
+                        }
+                    }
+                    //utility
+                    somethingInRow(leftUp, rightDown, leftUpInRow, rightDownInRow, player);
+                    somethingInRow(leftDown, rightUp, leftDownInRow, rightUpInRow, player);
                 }
-                //utility
-                utility += somethingInRow(leftUp,rightDown,leftUpInRow,rightDownInRow,player);
-                utility += somethingInRow(leftDown,rightUp,leftDownInRow,rightUpInRow,player);
                 //</editor-fold>
+                if (iWin) utility++;
+                if (uWin) utility--;
             }
         }
         return utility;
     }
-    private int somethingInRow(int a, int b, int aInRow, int bInRow, int player){
-        int sign = 0;
-        int utility = 0;
+
+    private void whoWins(int sign) {
+        if(sign==1){iWin = true;} else {uWin = true;}
+    }
+    private boolean bothWon(){
+        return (iWin == true && uWin == true);
+    }
+
+    private void somethingInRow(int a, int b, int aInRow, int bInRow, int player){
+        int sign;
         if (a != 0 && a == b) {
             sign = (a == player) ? 1 : -1;
-            utility += (aInRow + bInRow >= 3) ? sign : 0;
+            if (aInRow + bInRow >= 3) whoWins(sign);
+            //utility += (aInRow + bInRow >= 3) ? sign : 0;
         } else {
             sign = (a == player) ? 1 : -1;
-            utility += (aInRow >= 3) ? sign : 0;
+            if (aInRow >= 3) whoWins(sign);
+            //utility += (aInRow >= 3) ? sign : 0;
             sign = (b == player) ? 1 : -1;
-            utility += (bInRow >= 3) ? sign : 0;
+            if (bInRow >= 3) whoWins(sign);
+            //utility += (bInRow >= 3) ? sign : 0;
         }
-        return utility;
     }
 }
